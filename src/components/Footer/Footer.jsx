f55978a7ef0644f3801f3b9f56909e7eda74c3c4
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2024-12-24 16:10:29 +0600

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import footerLogo from "../../assets/logo.svg";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
+
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear(); // Get the current year dynamically
+  return (
+    <div>
+      <div className="container">
+        <div data-aos="zoom-in" className="grid md:grid-cols-3 pb-44 pt-5">
+          {/* company details */}
+          <div className="py-8 px-4">
+            <h1
+              data-aos="fade-up"
+              className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3"
+            >
+              <img src={footerLogo} alt="" className="max-w-[50px]" />
+              Sign of Modesty
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum in
+              beatae ea recusandae blanditiis veritatis.
+            </p>
+          </div>
+          {/* Footer Links */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Important Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-black dark:text-white"
+                      key={link.title}
+                    >
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-black dark:text-white"
+                      key={link.title}
+                    >
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            {/* social links */}
+            <div>
+              <div className="flex items-center gap-3 mt-6">
+                <a href="#">
+                  <FaInstagram className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaFacebook className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaLinkedin className="text-3xl" />
+                </a>
+              </div>
+              <div className="mt-6">
+                <div className="flex items-center gap-3 mt-3">
+                  <FaLocationArrow />
+                  <p>Kamrangirchar Dhaka</p>
+                </div>
+                <div className="flex items-center gap-3 mt-3">
+                  <FaMobileAlt />
+                  <p>+880 17-904770889</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Copyright Section */}
+        <div className="text-center py-4 ">
+          <p>© {currentYear} Sign of Modesty. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
